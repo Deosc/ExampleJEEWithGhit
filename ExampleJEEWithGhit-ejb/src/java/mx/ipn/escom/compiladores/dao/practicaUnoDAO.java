0bade2659b551bef200b169ad92d859e3e4cf8b0
Daniel <>
2017-08-08 16:56:52 -0500

    Cuarto commit.

---
+++ ExampleJEEWithGhit-ejb/src/java/mx/ipn/escom/compiladores/dao/practicaUnoDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.ipn.escom.compiladores.dao;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author oz_co
+ */
+@Stateless
+@LocalBean
+public class practicaUnoDAO {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
